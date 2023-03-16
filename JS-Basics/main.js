cea3a91003dfe600a9d1ee9f8af9d2c8d50d4e53
nabendu82 <nabendu.biswas@gmail.com>
2023-03-16 07:44:54 +0530

    JavaScript 2

--- JS-Basics/main.js
+++ JS-Basics/main.js
@@ -27,16 +27,82 @@
 // num3 = 35;
 
 //Data Types - String, Numbers, Boolean, null, undefined, Symbol.
-const name = 'Nabendu';
-const age = 40;
-const rating = 9.5;
-const isDeveloper = true;
-const x = null;
-let z;
+// const name = 'Nabendu';
+// const age = 40;
+// const rating = 9.5;
+// const isDeveloper = true;
+// const x = null;
+// let z;
 
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof rating);
-console.log(typeof isDeveloper);
-console.log(typeof x);
-console.log(typeof z);
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof rating);
+// console.log(typeof isDeveloper);
+// console.log(typeof x);
+// console.log(typeof z);
+
+//String concatenation
+// const name = 'Nabendu';
+// const age = 40;
+
+//old way
+// console.log('My name is ' + name + ' and i am ' + age + ' year\'s old.');
+
+//New way
+// console.log(`My name is ${name} and i am ${age} year's old.`);
+
+//String methods
+// const str = 'Hello JavaScript';
+
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.substring(0, 7));
+// console.log(str.includes('Java'));
+// console.log(str.includes('Cobol'));
+
+//Array
+// const lang = ['JavaScript', 'Java', 'C++', 'Golang', 'Kotlin', 5, true];
+// console.log(lang[2]);
+
+// lang[3] = 'C';
+// console.log(lang);
+
+// lang.pop();
+// lang.pop();
+// console.log(lang);
+
+// lang.push('Pascal');
+// lang.unshift('Swift');
+// console.log(lang);
+
+// lang.shift();
+// console.log(lang);
+
+// console.log(Array.isArray(lang));
+// console.log(Array.isArray('JavaScript'));
+// console.log(lang.includes('Java'));
+// console.log(lang.includes('Cobol'));
+
+//Objects
+const person = {
+    firstName: 'Nabendu',
+    'Last Name': 'Biswas',
+    age: 40,
+    isDeveloper: true,
+    hobbies: ['coding', 'youtubing','teaching'],
+    address: {
+        hno: 95,
+        city: 'Bhopal',
+        state: 'MP'
+    }
+}
+
+console.log(person);
+console.log(person.firstName);
+console.log(person['Last Name']);
+console.log(person.hobbies[1]);
+console.log(person.address.city);
+
+console.log(Object.keys(person));
+console.log(Object.values(person));

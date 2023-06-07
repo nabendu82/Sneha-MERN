6d3efc9306617f39a017a00a5c52b3ad794f55ce
nabendu82 <nabendu.biswas@gmail.com>
2023-06-07 07:32:30 +0530

    Redux-4

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
-import ClassTimer from './components/ClassTimer';
-import HookTimer from './components/HookTimer';
-// import GrandParent from './components/GrandParent';
+import GrandParent1 from './components/GrandParent1';
 
-// export const UserContext = React.createContext();
-// export const ChannelContext = React.createContext()
+export const CountContext = React.createContext()
+
+const initialState = 0;
+const reducer = (state, action) => {
+    switch(action){
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return state - 1;
+        case 'reset':
+            return initialState;
+        default:
+            return state;
+    }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className="App">
-      {/* <UserContext.Provider value="Nabendu">
-        <ChannelContext.Provider value="TWD">
-          <GrandParent />
-        </ChannelContext.Provider>
-      </UserContext.Provider> */}
-      <ClassTimer />
-      <HookTimer />
+      <CountContext.Provider value={{count, dispatch}}>
+          <h1>App Count - {count}</h1>
+          <GrandParent1 />
+      </CountContext.Provider>
     </div>
   );
 }
